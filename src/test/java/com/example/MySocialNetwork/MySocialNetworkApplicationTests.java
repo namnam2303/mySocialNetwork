ba13943d84f2b3436e2b2e8b688f337d5a01cacc
namnam2303 <imsofh@gmail.com>
2024-08-06 20:25:58 +0700

    the first commit

---
+++ src/test/java/com/example/MySocialNetwork/MySocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
